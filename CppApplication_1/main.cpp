dd030efc78bac190d5168502978ca07b2b20be15
aleskate <gavime@hotmail.es>
2013-10-27 07:53:40 -0600

    con neatbeans

---
+++ CppApplication_1/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: ale
+ *
+ * Created on 27 de octubre de 2013, 7:41
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
